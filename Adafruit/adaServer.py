80269f0f4c0f5b9f28abd65b93666575d92b5f52
hungpd90 <67276482+hungpd90@users.noreply.github.com>
2021-12-02 23:53:55 +0700

    Add files via upload

--- Adafruit/adaServer.py
+++ Adafruit/adaServer.py
@@ -1,19 +1,26 @@
 from Adafruit_IO import*
+from database import *
 import json
-aio = Client('vhhung', 'aio_kKKP15QsEGorIzlZemZCBwzyi9x7') # Tự test với server 
-aio2= Client('hungpham9469', 'aio_ytyr44KXjO5xLSMDC6dZqd9qBt6u')
+aio2 = Client('vhhung', 'aio_kKKP15QsEGorIzlZemZCBwzyi9x7') # Tự test với server 
+aio= Client('hungpham9469', 'aio_ytyr44KXjO5xLSMDC6dZqd9qBt6u')
 
 def Light_Turn_On_Strong():
     test_feed = aio.feeds('bk-iot-led') #truyền vô key của thiết bị
     aio.send_data(test_feed.key,'{ "id":"1", "name":"LED", "data":"2", "unit":"" }') 
+    sql = 'insert into LightHistory(FeedData) values(2)'
+    executeQuery(sql)
    
 def Light_Turn_On():
     test_feed = aio.feeds('bk-iot-led') #truyền vô key của thiết bị
     aio.send_data(test_feed.key,'{ "id":"1", "name":"LED", "data":"1", "unit":"" }') #truyền vô data
-    
+    sql = 'insert into LightHistory(FeedData) values(1)'
+    executeQuery(sql)
+
 def Light_Turn_Off():
     test_feed = aio.feeds('bk-iot-led')
     aio.send_data(test_feed.key,'{ "id":"1", "name":"LED", "data":"0", "unit":"" }')
+    sql = 'insert into LightHistory(FeedData) values(0)'
+    executeQuery(sql)
     
 def Fan_Turn_On_Strong():
     test_feed = aio.feeds('bk-iot-drv')
@@ -35,4 +42,35 @@ def Get_Temp():
 def Get_Light():
     test_feed = aio.feeds('bk-iot-light') #key thiết bị
     data = aio.receive(test_feed.key)
-    return json.loads(data.value)['data'] # json, lấy vùng dữ liệu
+    return json.loads(data.value)['data'] # json, lấy vùng dữ liệ
+#test function  
+def Get_All_Data_From_Feed(id):
+    data = aio.data(id)
+    json_data = []
+    for d in data:
+        json_data.append(json.loads(d.value))
+        #print("Data value: {0}".format(d.value))
+    return json_data
+
+def sub(id):
+    test_feed = aio.feeds(id)
+    data = aio.receive(test_feed.key)
+    #print('Latest value from Test: {0}'.format(data.value))
+    return json.loads(data.value)['data'] #json
+def pub(id,data):
+    test_feed = aio.feeds(id)
+    aio.send_data(test_feed.key, data)
+
+def sub2(id):
+    test_feed = aio2.feeds(id)
+    data = aio2.receive(test_feed.key)
+    #print('Latest value from Test: {0}'.format(data.value))
+    return json.loads(data.value)['data'] #json
+
+def pub2(id,data):
+    test_feed = aio2.feeds(id)
+    aio2.send_data(test_feed.key, data)
+
+
+
+
