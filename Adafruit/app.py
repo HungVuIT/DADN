7e48a8dc56fcbae285b49d0856df1a46a34c42c0
HungVuIT <67231540+HungVuIT@users.noreply.github.com>
2021-12-04 18:45:46 +0700

    add user manager and login

--- Adafruit/app.py
+++ Adafruit/app.py
@@ -16,17 +16,100 @@ def main():
     data = executeQuery(sql)
     return render_template('userManager.html',data=data)
 
-@app.route('/check',methods=['GET', 'POST'])
-def login():
-    # read the posted values from the UI
-    _user = request.form['user']
-    _password = request.form['pass']
-    # validate the received values
-    if data[_user]==_password:
-        return render_template('main.html')
-    else:
-        return render_template('login.html')
+#########################################################################
+###############           LOGIN              
+#########################################################################
 
+@app.route('/login', methods=["POST", "GET"])
+def Login():
+    if request.method == 'POST':
+        user = request.form['user']
+        password = request.form['pass']
+        if user == "admin" and password == "123456":
+            return render_template('admin.html')
+        else:
+            sql = "SELECT * FROM Persons WHERE tk = '"+user+"' and mk = '"+ password +"'"
+            user = getQuery(sql)
+            user = user.fetchone()
+            if user == None:
+                return render_template('login.html')
+            else:
+                return render_template('demo.html',user=user)
+
+#########################################################################
+###############           USER MANAGER              
+#########################################################################
+
+@app.route('/userManager')
+def userManager():
+    sql = "SELECT * FROM Persons ORDER BY id"
+    user = getQuery(sql)
+    return render_template('userManager.html', user=user)
+
+
+@app.route("/ajax_add", methods=["POST", "GET"])
+def ajax_add():
+    if request.method == 'POST':
+        txtname = request.form['txtname']
+        txtdepartment = request.form['txtdepartment']
+        txtusername = request.form['txtusername']
+        txtpassword = request.form['txtpassword']
+        print(txtname)
+        if txtname == '':
+            msg = 'Please Input Name'
+        elif txtdepartment == '':
+            msg = 'Please Input Department'
+        elif txtusername == '':
+            msg = 'Please Input User Name'
+        elif txtpassword == '':
+            msg = 'Please Input Password'
+        else:
+            executeQuery("INSERT INTO Persons (name,tk,mk,room) VALUES ('" + txtname +
+                         "','" + txtusername + "','" + txtpassword + "','" + txtdepartment+"')")
+            msg = 'New record created successfully'
+    return jsonify(msg)
+
+
+@app.route("/ajax_update", methods=["POST", "GET"])
+def ajax_update():
+    if request.method == 'POST':
+        string = request.form['string']
+        print(string)
+        txtname = request.form['txtname']
+        txtdepartment = request.form['txtdepartment']
+        txtusername = request.form['txtusername']
+        print('6')
+        txtpassword = request.form['txtpassword']
+        print(string)
+        executeQuery("UPDATE Persons SET name = '" + txtname + "' , tk = '" + txtusername +
+                     "', mk =  '" + txtpassword + "', room =  '" + txtdepartment + "' WHERE id =  " + string + " ")
+        msg = 'Record successfully Updated'
+    return jsonify(msg)
+
+
+@app.route("/ajax_delete", methods=["POST", "GET"])
+def ajax_delete():
+    if request.method == 'POST':
+        getid = request.form['string']
+        print(getid)
+        executeQuery('DELETE FROM Persons WHERE id = {0}'.format(getid))
+        msg = 'Record deleted successfully'
+    return jsonify(msg)
+
+
+#########################################################################
+###############           HISTORY              
+#########################################################################
+
+@app.route("/showHistory")
+def showHistory():
+    #return render_template('showHistory.html', data=Get_All_Data_From_Feed("bk-iot-led"), data2=Get_All_Data_From_Feed("bk-iot-drv"))
+    sql='select * from LightHistory;'
+    data = getQuery(sql)
+    sql2='select * from FanHistory;'
+    #datafan = getQuery(sql2)
+    return render_template('showHistory.html',data=data)
+  
 
 @app.route('/on')
 def turnOn():
@@ -40,13 +123,6 @@ def turnOff():
     dosang = sub('light')
     return render_template('index.html',mode=dosang)
 
-@app.route("/showHistory")
-def showHistory():
-    #return render_template('showHistory.html', data=Get_All_Data_From_Feed("bk-iot-led"), data2=Get_All_Data_From_Feed("bk-iot-drv"))
-    sql='select * from LightHistory;'
-    data = getQuery(sql)
-    sql2='select * from FanHistory;'
-    #datafan = getQuery(sql2)
-    return render_template('showHistory.html',data=data)
+
 if __name__ == "__main__":
     app.run()
