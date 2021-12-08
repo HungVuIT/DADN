e1667bcc5f510dbb13cc8522d3cc05607d0eac07
HungVuIT <67231540+HungVuIT@users.noreply.github.com>
2021-12-08 18:47:52 +0700

    NEW

--- Adafruit/app.py
+++ Adafruit/app.py
@@ -2,15 +2,12 @@ from flask import*
 from adaServer import *
 from database import *
 from flask_socketio import SocketIO
-data = {
-    "room1": "123456",
-    "room2": "123456",
-    "room3": "123456"
-}
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 socketio = SocketIO(app)
 
+name=""
 
 @app.route('/')
 def admin():
@@ -27,7 +24,9 @@ def room1():
     data = getQuery(sql).fetchone()
     led = Get_Led()
     fan = Get_Fan()
-    return render_template('user.html', room=data, led=led, fan=fan)
+    temp = Get_Temp()
+    humid= Get_Humid()
+    return render_template('user.html', room=data, led=led, fan=fan, temp=temp, humid=humid)
 
 
 @app.route('/room2')
@@ -36,7 +35,9 @@ def room2():
     data = getQuery(sql).fetchone()
     led = Get_Led()
     fan = Get_Fan()
-    return render_template('user.html', room=data, led=led, fan=fan)
+    temp = Get_Temp()
+    humid= Get_Humid()
+    return render_template('user.html', room=data, led=led, fan=fan, temp=temp, humid=humid)
 
 
 @app.route('/room3')
@@ -45,7 +46,9 @@ def room3():
     data = getQuery(sql).fetchone()
     led = Get_Led()
     fan = Get_Fan()
-    return render_template('user.html', room=data, led=led, fan=fan)
+    temp = Get_Temp()
+    humid= Get_Humid()
+    return render_template('user.html', room=data, led=led, fan=fan, temp=temp, humid=humid)
 
 
 @app.route('/room4')
@@ -54,7 +57,9 @@ def room4():
     data = getQuery(sql).fetchone()
     led = Get_Led()
     fan = Get_Fan()
-    return render_template('user.html', room=data, led=led, fan=fan)
+    temp = Get_Temp()
+    humid= Get_Humid()
+    return render_template('user.html', room=data, led=led, fan=fan, temp=temp, humid=humid)
 
 
 @app.route('/room5')
@@ -63,12 +68,20 @@ def room5():
     data = getQuery(sql).fetchone()
     led = Get_Led()
     fan = Get_Fan()
-    return render_template('user.html', room=data, led=led, fan=fan)
+    temp = Get_Temp()
+    humid= Get_Humid()
+    return render_template('user.html', room=data, led=led, fan=fan, temp=temp, humid=humid)
 
 
 #########################################################################
 # CHAT
 #########################################################################
+
+@app.route('/chat')
+def sessions():
+    global name
+    return render_template('chat.html',name=name)
+
 
 def messageReceived(methods=['GET', 'POST']):
     print('message was received!!!')
@@ -98,12 +111,16 @@ def Login():
             if user == None:
                 return render_template('login.html')
             else:
+                global name
+                name = user.name
                 id = user.room
                 sql = 'select * from Room where Id ='+id+' ;'
                 data = getQuery(sql).fetchone()
                 led = Get_Led()
                 fan = Get_Fan()
-                return render_template('user.html', room=data, led=led, fan=fan)
+                temp = Get_Temp()
+                humid= Get_Humid()
+                return render_template('user.html', room=data, led=led, fan=fan, temp=temp, humid=humid)
 
 #########################################################################
 # USER MANAGER
@@ -170,8 +187,7 @@ def ajax_delete():
 # AUTO MODE
 #########################################################################
 
-
-@app.route('/autoOn')
+@app.route('/auto', methods=["POST", "GET"])
 def auto():
     # read the posted values from the UI
     lihgtnow = int(Get_Light())
@@ -180,7 +196,7 @@ def auto():
     else:
         Led_Turn_Off()
     led = Get_Led()
-    return render_template('demo.html', led=led)
+    return jsonify(led)
 
 #########################################################################
 # FAN AND LED BTN
@@ -250,30 +266,5 @@ def showHistory():
     return render_template('user.html', light_data=data, fan_data=data2, temp_data=data3)
 
 
-"""
-@app.route('/check',methods=['GET', 'POST'])
-def login():
-    # read the posted values from the UI
-    _user = request.form['user']
-    _password = request.form['pass']
-    # validate the received values
-    if data[_user]==_password:
-        return render_template('main.html')
-    else:
-        return render_template('login.html')
-
-
-@app.route('/on')
-def turnOn():
-    pub('led','{"id":"1","name":"LED","data":"1","unit":""}')
-    dosang = sub('light')
-    return render_template('index.html',mode=dosang)
-
-@app.route('/off')
-def turnOff():
-    pub('led','{"id":"1","name":"LED","data":"0","unit":""}')
-    dosang = sub('light')
-    return render_template('index.html',mode=dosang)
-"""
 if __name__ == "__main__":
     socketio.run(app, debug=True)
--- Adafruit/db.sql
+++ Adafruit/db.sql
@@ -2,12 +2,12 @@ create database ADADB
 use ADADB
 
 CREATE TABLE Persons(
-    Id int NOT NULL IDENTITY(1,1),
-    Name varchar(255),
+    id int NOT NULL IDENTITY(1,1),
+    name varchar(255),
     tk varchar(255),
-	mk int,
-	Room int,
-    PRIMARY KEY (Id),
+	mk varchar(255),
+	room varchar(255),
+    PRIMARY KEY (id),
 );
 
 INSERT INTO Persons (Name,tk,mk,Room)
