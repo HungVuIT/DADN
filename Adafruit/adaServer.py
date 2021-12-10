7be4bef0ea8b5cff2acf87bf301a44d824836de0
hungpd90 <67276482+hungpd90@users.noreply.github.com>
2021-12-10 15:18:54 +0700

    NEW

--- Adafruit/adaServer.py
+++ Adafruit/adaServer.py
@@ -8,26 +8,37 @@ aio1= Client('hungpham9469', 'aio_ytyr44KXjO5xLSMDC6dZqd9qBt6u')
 def Led_Turn_On():
     test_feed = aio.feeds('bk-iot-led') 
     aio.send_data(test_feed.key,'{ "id":"1", "name":"LED", "data":"1", "unit":"" }') 
+    sql = 'insert into LightHistory(feed_data) values(1)'
+    executeQuery(sql)
     
 def Led_Turn_Off():
     test_feed = aio.feeds('bk-iot-led')
     aio.send_data(test_feed.key,'{ "id":"1", "name":"LED", "data":"0", "unit":"" }')
+    sql = 'insert into LightHistory(feed_data) values(0)'
+    executeQuery(sql)
 
 def Fan_Turn_On_3():
     test_feed = aio.feeds('bk-iot-drv')
     aio.send_data(test_feed.key,'{ "id":"10", "name":"DRV_PWM", "data":"200", "unit":"" }')
-
+    sql = 'insert into FanHistory(feed_data) values(200)'
+    executeQuery(sql)
 def Fan_Turn_On_2():
     test_feed = aio.feeds('bk-iot-drv')
     aio.send_data(test_feed.key,'{ "id":"10", "name":"DRV_PWM", "data":"150", "unit":"" }')
+    sql = 'insert into FanHistory(feed_data) values(150)'
+    executeQuery(sql)
     
 def Fan_Turn_On_1():
     test_feed = aio.feeds('bk-iot-drv')
     aio.send_data(test_feed.key,'{ "id":"10", "name":"DRV_PWM", "data":"100", "unit":"" }')
+    sql = 'insert into FanHistory(feed_data) values(100)'
+    executeQuery(sql)
 
 def Fan_Turn_Off():
     test_feed = aio.feeds('bk-iot-drv')
     aio.send_data(test_feed.key,'{ "id":"10", "name":"DRV_PWM", "data":"0", "unit":"" }')
+    sql = 'insert into FanHistory(feed_data) values(0)'
+    executeQuery(sql)
   
 def Get_Led(): # Lấy trạng thái hiện tại của đèn led
     test_feed = aio.feeds('bk-iot-led') #key thiết bị
